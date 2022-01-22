99dab5a3253ddb107cb4d346e160f7bd8f05656a
stellagosa <stellagosa@126.com>
2022-01-22 12:59:37 +0800

    1332

---
+++ src/main/java/com/stellagosa/leetcode/leetcode_1332/Solution.java
@@ -0,0 +1,18 @@
+package com.stellagosa.leetcode.leetcode_1332;
+
+/**
+ * @Author: Stellagosa
+ * @Date: 2022/1/22 12:16
+ * @Description: 删除回文子序列
+ */
+public class Solution {
+
+    // 子序列可以不连续
+    public int removePalindromeSub(String s) {
+        if (s.length() == 0) return 0;
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - i - 1)) return 2;
+        }
+        return 1;
+    }
+}
