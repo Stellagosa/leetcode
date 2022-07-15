fcb3ffdab4549b1909601caa0f9f8a3314467efe
Stellagosa <Stellagosa@126.com>
2022-07-16 05:54:20 +0800

    Offer II 041 滑动窗口的平均值

---
+++ src/main/java/com/stellagosa/offer_II/offer_041/MovingAverage.java
@@ -0,0 +1,39 @@
+package com.stellagosa.offer_II.offer_041;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Author: Stellagosa
+ * @Date: 2022/7/16 5:30
+ * @Description: 滑动窗口的平均值
+ */
+public class MovingAverage {
+
+    public static void main(String[] args) {
+        MovingAverage o = new MovingAverage(3);
+        int[] arr = {1,10, 3, 5};
+        for (int num : arr) {
+            System.out.println(o.next(num));
+        }
+    }
+
+    private final Queue<Integer> queue;
+    private final int size;
+    private double count;
+
+    public MovingAverage(int size) {
+        this.queue = new LinkedList<>();
+        this.size = size;
+        this.count = 0;
+    }
+
+    public double next(int val) {
+        if (queue.size() == this.size) {
+            this.count -= queue.remove();
+        }
+        this.queue.offer(val);
+        this.count += val;
+        return count / this.queue.size();
+    }
+}
