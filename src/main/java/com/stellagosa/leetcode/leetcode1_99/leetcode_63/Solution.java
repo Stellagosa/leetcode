60f683cb129e5fb5691502845ef88714a8ff42e1
stellagosa <stellagosa@126.com>
2023-06-02 18:38:30 +0800

    63 不同路径II

---
+++ src/main/java/com/stellagosa/leetcode/leetcode1_99/leetcode_63/Solution.java
@@ -0,0 +1,35 @@
+package com.stellagosa.leetcode.leetcode1_99.leetcode_63;
+
+/**
+ * @Author: Stellagosa
+ * @Date: 2023/6/2 下午 06:17
+ * @Description: 不同路径 II
+ */
+public class Solution {
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid[0][0] == 1) return 0;
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 0)
+                dp[i][0] = dp[i - 1][0];
+
+        }
+        for (int i = 1; i < n; i++) {
+            if (obstacleGrid[0][i] == 0)
+                dp[0][i] = dp[0][i - 1];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0)
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+}
