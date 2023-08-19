4a29d1b94b7267595c38ca1ca6432fe6086c8a74
stellagosa <stellagosa@126.com>
2023-08-19 08:31:27 +0800

    2235 两整数相加

---
+++ src/main/java/com/stellagosa/leetcode/leetcode2000_2999/leetcode_2235/Solution.java
@@ -0,0 +1,13 @@
+package com.stellagosa.leetcode.leetcode2000_2999.leetcode_2235;
+
+/**
+ * @Author: Stellagosa
+ * @Date: 2023/8/19 8:30
+ * @Description: 两整数相加
+ */
+public class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+
