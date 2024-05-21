84e7b8a7392225c02cabbdd5d89364cfb8d797f3
Stellagosa <Stellagosa@126.com>
2024-05-21 09:13:02 +0800

    2769 找出最大的可达成数字

---
+++ src/main/java/com/stellagosa/leetcode/leetcode2000_2999/leetcode_2769/Solution.java
@@ -0,0 +1,13 @@
+package com.stellagosa.leetcode.leetcode2000_2999.leetcode_2769;
+
+/**
+ * @author Stellagosa
+ * @description 找出最大的可达成数字
+ * @date 2024/5/21 9:11 星期二
+ */
+public class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + 2 * t;
+    }
+
+}
