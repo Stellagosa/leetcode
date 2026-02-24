17f4a13b9a551b7f0f24ea765a82274292bd869d
Stellagosa <Stellagosa@126.com>
2026-02-24 21:04:01 +0800

    1022 从根到叶的二进制数之和

--- src/main/java/com/stellagosa/leetcode/leetcode1000_1999/leetcode_1022/Solution.java
+++ src/main/java/com/stellagosa/leetcode/leetcode1000_1999/leetcode_1022/Solution.java
@@ -3,7 +3,7 @@ package com.stellagosa.leetcode.leetcode1000_1999.leetcode_1022;
 /**
  * @Author: Stellagosa
  * @Date: 2022/5/30 7:09
- * @Description: 从根到叶的二进制之和
+ * @Description: 从根到叶的二进制数之和
  */
 public class Solution {
 
@@ -18,25 +18,40 @@ public class Solution {
         System.out.println(solution.sumRootToLeaf(root));
     }
 
-    // TAG 深度优先遍历
     public int sumRootToLeaf(TreeNode root) {
-        dfs(root, 0);
-        return sum;
+        return dfs(root, 0);
     }
 
-    private void dfs(TreeNode root, int val) {
-        if (root == null) return;
-        int temp = root.val + (val << 1);
+    private int dfs(TreeNode root, int num) {
+        if (root == null) return 0;
+        num = (num << 1) + root.val;
         if (root.left == null && root.right == null) {
-            sum += temp;
+            return num;
         }
-        if (root.left != null) {
-            dfs(root.left, temp);
-        }
-        if (root.right != null) {
-            dfs(root.right, temp);
-        }
+        return dfs(root.left, num) + dfs(root.right, num);
     }
+
+
+
+    // TAG 深度优先遍历
+//    public int sumRootToLeaf(TreeNode root) {
+//        dfs(root, 0);
+//        return sum;
+//    }
+
+//    private void dfs(TreeNode root, int val) {
+//        if (root == null) return;
+//        int temp = root.val + (val << 1);
+//        if (root.left == null && root.right == null) {
+//            sum += temp;
+//        }
+//        if (root.left != null) {
+//            dfs(root.left, temp);
+//        }
+//        if (root.right != null) {
+//            dfs(root.right, temp);
+//        }
+//    }
 
 
     private void treeGenerator(int[] arr, TreeNode root, int index) {
